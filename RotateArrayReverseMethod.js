900d580ba0e14e5952ec111e77c1e0d0e700c741
Critter4Dinner <woffordben1992@gmail.com>
2020-01-26 12:11:08 -0500

    added commentary for rotate array reverse method

--- RotateArrayReverseMethod.js
+++ RotateArrayReverseMethod.js
@@ -1,18 +1,18 @@
 const rotate = (array, rotationAmount) => {
-  rotationAmount = rotationAmount % array.length;
-  reverse(array, 0, array.length - 1);
-  reverse(array, 0, rotationAmount - 1);
-  reverse(array, rotationAmount, array.length - 1);
-  return console.log(array);
+  rotationAmount = rotationAmount % array.length; // With this, we can have a rotation amount larger than the array length
+  reverse(array, 0, array.length - 1); // Reverse from the beginning to the end of the array
+  reverse(array, 0, rotationAmount - 1); // Reverse from the beginning to the rotation amount index
+  reverse(array, rotationAmount, array.length - 1); // Reverse from the rotation amount index to the end of the array
+  return console.log(array); 
 };
 
 const reverse = (array, startingIndex, endingIndex) => {
   while (startingIndex < endingIndex) {
-    temporaryBox = array[startingIndex];
-    array[startingIndex] = array[endingIndex];
-    array[endingIndex] = temporaryBox;
-    startingIndex++;
-    endingIndex--;
+    temporaryBox = array[startingIndex]; // store the first number from the array in a box because we are switching it around, but we will still need it after the switch. 
+    array[startingIndex] = array[endingIndex]; // In the array, switch the last number with the first number.
+    array[endingIndex] = temporaryBox; // Set the last number to the first number of the array (the number before we did the switcheroo)
+    startingIndex++; // increment so we can do the same thing with the inner numbers of the array
+    endingIndex--; // decrement so we can do the same thing with the inner numbers of the array
   }
   return array;
 };
