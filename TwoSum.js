e9ea44105bf7f475d403b5dcc118856f16ab8a7f
Critter4Dinner <woffordben1992@gmail.com>
2020-01-29 08:41:50 -0500

    Fixed bug where j was equal to one in the nested for loop. Updated it to where it is now equal to i + 1

--- TwoSum.js
+++ TwoSum.js
@@ -1,6 +1,6 @@
 const sumReturnIndices = (array, targetSum) => {
   for (let i = 0; i < array.length; i++) {
-    for (let j = 1; j < array.length; j++) {
+    for (let j = i + 1; j < array.length; j++) {
       if (array[i] + array[j] === targetSum) {
         return console.log([i, j]);
       }
